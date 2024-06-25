5e6574c2582f7c2908666580fdf5fb0dff62ea58
Mahmoud Elfar <farx87@gmail.com>
2024-06-25 03:52:58 -0700

    chore: Update UAMSim environment registration in __init__.py

--- uam_sim/__init__.py
+++ uam_sim/__init__.py
@@ -6,6 +6,14 @@ The UAMSimEnv environment is a simulation of an urban air mobility (UAM) system.
 
 This file registers the UAMSimEnv environment with OpenAI Gymnasium and exports the `gymnasium.make` function.
 
+Each entry in the `__all__` list is a string that represents the name of an environment. For example: 'UAMSim' corresponds to the UAMSimEnv environment that is implemented in the `uam_sim.envs.uam_sim` module. So:
+- Name: 'SomeName'
+- Implementation: 'uam_sim/envs/some_name.py'
+- Module: 'uam_sim.envs.some_name'
+- Class: 'SomeNameEnv'
+- ID: 'SomeName-v0'
+- Entry point: 'uam_sim.envs:SomeName'
+
 """
 __version__ = '0.0.1'
 __all__ = ['UAMSim']  # In the future, add more environments here
@@ -17,6 +25,7 @@ from gymnasium.envs.registration import register
 
 try:
     for e in __all__:
+        # TODO - FAR: Check if this limit is necessary
         env_name = e[:-9]  # Limit the name to the first 9 characters
         register(
             id=f'{env_name}-v0',
